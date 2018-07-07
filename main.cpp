83d9b33deca89d2a07e03cc5b3239584383a36b8
skyline <skyline@localhost.localdomain>
2018-07-07 15:05:21 +0800

    添加路径设置

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <QStyleFactory>
 #include <QDebug>
 #include <QFileDialog>
+#include <QSettings>
 
 #include "candroiddevice.h"
 
@@ -20,57 +21,63 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QCoreApplication::setOrganizationName("LeZheng");
+    QCoreApplication::setOrganizationDomain("lezheng.com");
+    QCoreApplication::setApplicationName("Canary adb");
+
     QTranslator translator;
     translator.load("canary_zh_CN.qm");
     a.installTranslator(&translator);
 
     QString defaultToolPath = QCoreApplication::applicationDirPath() + QDir::separator() + "platform-tools";
-    while(!QDir(defaultToolPath).exists() || !QFile(defaultToolPath + QDir::separator() + adbFileName).exists()) {
+
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+    QSettings setting;
+    QString adbPath = setting.value("anroid/adb/path",defaultToolPath + QDir::separator() + adbFileName).toString();
+    while(!QFile(adbPath).exists()) {
         QString sdkPath = QFileDialog::getExistingDirectory(nullptr, a.tr("Choose Android SDK Directory"),
                           QDir::rootPath(),
                           QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
         if(sdkPath.isEmpty() || !QDir(sdkPath).exists()) {
-            break;
+            return 0;
         } else {
             if(sdkPath.endsWith("platform-tools")) {
                 defaultToolPath = sdkPath;
             } else {
                 defaultToolPath = sdkPath + QDir::separator() + "platform-tools";
             }
+            adbPath = defaultToolPath + QDir::separator() + adbFileName;
         }
     }
-    CAndroidContext::androidAdbPath = defaultToolPath + QDir::separator() + adbFileName;
+    setting.setValue("anroid/adb/path",adbPath);
+    CAndroidContext::androidAdbPath = adbPath;
 
-    if(QFile(CAndroidContext::androidAdbPath).exists()){
-        MainWindow *window = new MainWindow;
-        window->move((a.desktop()->width() - window->width()) / 2, (a.desktop()->height() - window->height()) / 2);
+    MainWindow *window = new MainWindow;
+    window->move((a.desktop()->width() - window->width()) / 2, (a.desktop()->height() - window->height()) / 2);
 
-        QPixmap pixmap(":/img/startup_bg");
-        QSplashScreen *splash = new QSplashScreen(pixmap.scaled(400,270));
-        splash->resize(400,270);
-        splash->show();
-        splash->showMessage(splash->tr("start init..."),Qt::AlignBottom,Qt::white);
-        a.processEvents();
-        CAndroidContext::getInstance()->startListenAdb();
-        a.connect(CAndroidContext::getInstance(),&CAndroidContext::updateStateChanged,splash,[splash](const QString &msg) {
-            splash->showMessage(msg,Qt::AlignBottom,Qt::white);
-        });
-        a.connect(CAndroidContext::getInstance(),&CAndroidContext::findNewDevice,splash,[splash](CAndroidDevice *device) {
-            splash->showMessage(splash->tr("init device:%1 ...").arg(device->serialNumber),Qt::AlignBottom,Qt::white);
-        });
-        a.connect(CAndroidContext::getInstance(),&CAndroidContext::deviceListUpdated,splash,[splash,window]() {
-            window->show();
-            splash->finish(window);
-            delete splash;
-        });
-        a.connect(CAndroidContext::getInstance(),&CAndroidContext::deviceListNotUpdated,splash,[splash,window]() {
-            window->show();
-            splash->finish(window);
-            delete splash;
-        });
+    QPixmap pixmap(":/img/startup_bg");
+    QSplashScreen *splash = new QSplashScreen(pixmap.scaled(400,270));
+    splash->resize(400,270);
+    splash->show();
+    splash->showMessage(splash->tr("start init..."),Qt::AlignBottom,Qt::white);
+    a.processEvents();
+    CAndroidContext::getInstance()->startListenAdb();
+    a.connect(CAndroidContext::getInstance(),&CAndroidContext::updateStateChanged,splash,[splash](const QString &msg) {
+        splash->showMessage(msg,Qt::AlignBottom,Qt::white);
+    });
+    a.connect(CAndroidContext::getInstance(),&CAndroidContext::findNewDevice,splash,[splash](CAndroidDevice *device) {
+        splash->showMessage(splash->tr("init device:%1 ...").arg(device->serialNumber),Qt::AlignBottom,Qt::white);
+    });
+    a.connect(CAndroidContext::getInstance(),&CAndroidContext::deviceListUpdated,splash,[splash,window]() {
+        window->show();
+        splash->finish(window);
+        delete splash;
+    });
+    a.connect(CAndroidContext::getInstance(),&CAndroidContext::deviceListNotUpdated,splash,[splash,window]() {
+        window->show();
+        splash->finish(window);
+        delete splash;
+    });
 
-        return a.exec();
-    }else{
-        return 0;
-    }
+    return a.exec();
 }
